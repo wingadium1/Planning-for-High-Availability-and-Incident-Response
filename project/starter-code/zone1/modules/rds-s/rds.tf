4eb2f411fc06f4bfbbdf682f8ac70d219eb04b58
Son Hoang <wingadium1@gmail.com>
2024-05-15 14:05:51 +0700

    Add rds engine

--- project/starter-code/zone1/modules/rds-s/rds.tf
+++ project/starter-code/zone1/modules/rds-s/rds.tf
@@ -42,6 +42,7 @@ resource "aws_rds_cluster_instance" "udacity_instance-s" {
   cluster_identifier   = aws_rds_cluster.udacity_cluster-s.id
   instance_class       = "db.t2.small"
   db_subnet_group_name = aws_db_subnet_group.udacity_db_subnet_group.name
+  engine               = "mysql"
 }
 
 resource "aws_security_group" "db_sg_2" {
